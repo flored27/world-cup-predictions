c7d682d64c499e6830b312ce222622fbc7c6a654
flored27 <dflores@fandm.edu>
2018-06-25 18:10:09 -0400

    added bracket to app.js

--- world-cup-statistics/src/App.js
+++ world-cup-statistics/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BracketDetail from './components/presentational/bracketdetail'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <BracketDetail/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
